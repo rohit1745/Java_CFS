da5257c8f89a81dc0c12b492e78c6714a3f20cc5
rohit1745 <rohitbadgujar535@gmail.com>
2026-03-21 10:30:12 +0530

    first commit

---
+++ OOPs/Encapsulation.java
@@ -0,0 +1,16 @@
+package OOPs;
+
+public class Encapsulation {
+    private int AccountNo;
+    private double AccountBal;
+    public int getAcccountNo(){
+        return AccountNo;
+    }
+    class Demo
+    {
+        public static void main (String args[])
+        {
+            
+        }
+    }    
+}
