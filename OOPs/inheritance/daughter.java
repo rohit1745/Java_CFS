663953a02eb5b6282e5d9329207b9e610466fac6
rohit1745 <rohitbadgujar535@gmail.com>
2026-03-21 10:28:25 +0530

    first commit

---
+++ OOPs/inheritance/daughter.java
@@ -0,0 +1,12 @@
+package OOPs.inheritance;
+
+public class daughter extends child {
+    void gold()
+    {
+        System.out.println("gold is 50kg");
+    }
+    void Diamond()
+    {
+        System.out.println("Diamond are 10kg");
+    }
+}
