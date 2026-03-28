574d248153fa36b48cf9519dfe3874d8b6669d09
rohit1745 <rohitbadgujar535@gmail.com>
2026-03-28 20:40:36 +0530

    1st scanner class

---
+++ OOPs/Exception/input.java
@@ -0,0 +1,26 @@
+package OOPs.Exception;
+
+import java.util.Scanner;
+
+public class input {
+     
+    public static void main(String args[])
+    
+    {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter your Name"); 
+        String name=sc.nextLine();
+
+        System.out.println("Enter your Roll_No"); 
+        int Roll_No=sc.nextInt();
+        
+        System.out.println("Enter your City"); 
+        String City=sc.nextLine();
+
+        
+       
+        
+    }
+    
+}
