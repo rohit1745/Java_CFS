d575e16e37e13bb5146c76f8694c975ed67d6d66
rohit1745 <rohitbadgujar535@gmail.com>
2026-05-03 11:27:25 +0530

    ListIterator Demo Program

---
+++ Collection/ListIlerator.java
@@ -0,0 +1,32 @@
+package Collection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ListIterator;
+
+public class ListIlerator {
+    public static void main(String[] args) {
+        List<Integer> list=new ArrayList<>(Arrays.asList(10,20,30,40,50,60));
+        System.out.println(list);
+
+        ListIterator<Integer> ListIterator = list.listIterator();
+        while(ListIterator.hasNext())
+        {
+            Integer data  = ListIterator.next();
+
+            if(data==10)
+            ListIterator.remove();
+            
+            if(data==30)
+            {
+            ListIterator.add(100);
+            }
+
+            
+        }
+        System.out.println(list);
+        
+        
+    }
+}
