f1ced6e55cf35a0d19cdffd4659414c1693ae3fc
rohit1745 <rohitbadgujar535@gmail.com>
2026-05-06 15:37:46 +0530

    Generics Main Program

--- WrapperANDGenerics/Generics/main.java
+++ WrapperANDGenerics/Generics/main.java
@@ -1,5 +1,24 @@
 package WrapperANDGenerics.Generics;
 
+
+class pair<K,V>
+{
+    K key;
+    public pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+    V value;
+    void display()
+    {
+        System.out.println("key--->"+key +" value--->"+ value);
+    }
+
+    
+
+
+}
+
 public class main {
     public static void main(String[] args) {
 
@@ -10,6 +29,13 @@ public class main {
         Gift<String> stringGift=new Gift<>();
         stringGift.setValue("Rohit");
         System.out.println(stringGift.getValue());
+
+        pair<Integer,String> p1=new pair(1, "Ram");
+        pair<Integer,String> p2=new pair(2, "Sham");
+        p1.display();
+        p2.display();
+
+
 
     }
     
