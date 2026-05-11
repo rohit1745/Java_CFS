de6c3570e4c166de3ced20ee7cc1390b5f60ef36
rohit1745 <rohitbadgujar535@gmail.com>
2026-05-11 21:32:54 +0530

    Mini project to Library Management System

--- Advance_Java/untitled/src/Library_Management_System/DBConfigLib.java
+++ Advance_Java/untitled/src/Library_Management_System/DBConfigLib.java
@@ -1,4 +1,8 @@
 package Library_Management_System;
 
 public class DBConfigLib {
+    static String url="jdbc:mysql://localhost:3306/library_db";
+    static String username="root";
+    static String password="Rohit@1745@";
+    static String className="com.mysql.cj.jdbc.Driver";
 }
--- Advance_Java/untitled/src/Library_Management_System/library_management_sys.java
+++ Advance_Java/untitled/src/Library_Management_System/library_management_sys.java
@@ -1,4 +1,38 @@
 package Library_Management_System;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class library_management_sys {
+    public static void main(String[] args) {
+        try
+        {
+            Connection con= DriverManager.getConnection(DBConfigLib.url, DBConfigLib.username, DBConfigLib.password);
+            Scanner sc=new Scanner(System.in);
+
+            while (true)
+            {
+                System.out.println("1.Add Book");
+                System.out.println("1.View Books");
+                System.out.println("1.Add Book");
+                System.out.println("1.Add Book");
+                System.out.println("1.Add Book");
+                System.out.println("1.Add Book");
+                System.out.println("1.Add Book");
+
+
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        catch (SQLException s)
+        {
+            System.out.println("Data Error:"+s.getMessage());
+        }
+    }
 }
