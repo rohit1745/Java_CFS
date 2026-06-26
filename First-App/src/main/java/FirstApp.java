4b99481d74373d15c9c25e892499338db348d793
EknathBelge <esbelge5002@gmail.com>
2026-06-26 11:47:02 +0530

    first-App

---
+++ First-App/src/main/java/FirstApp.java
@@ -0,0 +1,18 @@
+import jakarta.servlet.GenericServlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FirstApp extends GenericServlet {
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        res.setContentType("html/text");
+
+
+        PrintWriter out=res.getWriter();
+        out.println("<h1> Hello DADA........</h1>");
+    }
+}
